1c38a4f500e1c995263730b5c23c1a23a775b0cb
Vadim Andreev <vadimcola@mail.ru>
2023-05-24 10:36:55 +0700

    Реализованы два контролера, отоброжаются две страницы home и contacts

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from catalog.views import index
+from catalog.views import *
 
 urlpatterns = [
-    path('', index)
+    path('', home),
+    path('contacts/', contacts)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'catalog/index.html')
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    return render(request, 'catalog/contacts.html')
